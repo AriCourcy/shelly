2aef9f7fff4c05f51b75e7ca24c810fc90d6408d
Ariane Courcy <acourcy@cshawi.ca>
2026-08-24 15:04:55 -0400

    improve pwa updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.tsx'
+
+registerSW({
+  immediate: true,
+  onRegisteredSW(swUrl, registration) {
+    console.log('Shelly service worker registered:', swUrl)
+
+    if (registration) {
+      setInterval(() => {
+        registration.update()
+      }, 60 * 1000)
+    }
+  },
+  onOfflineReady() {
+    console.log('Shelly is ready to work offline')
+  },
+  onNeedRefresh() {
+    console.log('Shelly update available')
+    window.location.reload()
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
